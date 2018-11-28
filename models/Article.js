be1f54cbd60677a914c0270f48097699f6520fd9
Tim Lewis <timothydlewis@icloud.com>
2018-11-27 17:32:44 -0700

    Removed excess comments

--- models/Article.js
+++ models/Article.js
@@ -6,12 +6,10 @@ var Schema = mongoose.Schema;
 // Using the Schema constructor, create a new UserSchema object
 // This is similar to a Sequelize model
 var ArticleSchema = new Schema({
-  // `title` is required and of type String
   title: {
     type: String,
     required: true
   },
-  // `link` is required and of type String
   link: {
     type: String,
     required: true
